f0e8b64f601f1d199202aaf015c9ba1423871cd3
haizhilangzi <415242678@qq.com>
2020-03-20 01:04:53 +0800

    init

---
+++ rookie-javase-thread/src/main/java/com/rookie/bigdata/thread/pool/newCachedThreadPoolMain.java
@@ -0,0 +1,65 @@
+package com.rookie.bigdata.thread.pool;
+
+import com.rookie.bigdata.thread.MyCallable;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author rookie
+ * @version 1.0
+ * @date 2020/3/19 23:23
+ */
+public class newCachedThreadPoolMain {
+
+    public static void main(String[] args) throws Exception{
+
+        //ExecutorService executorService = Executors.newCachedThreadPool();
+        //ExecutorService executorService=Executors.newFixedThreadPool(2, Executors.defaultThreadFactory());
+        //ExecutorService executorService=Executors.newSingleThreadExecutor();
+
+        //new MyRunnable("thread");
+//        for (int i = 0; i <= 100; i++) {
+//            executorService.submit(new MyRunnable("thread" + i));
+//        }
+//
+//        Thread.sleep(10000);
+//        executorService.shutdown();
+
+        ScheduledExecutorService scheduledExecutorService=Executors.newScheduledThreadPool(2);
+
+        scheduledExecutorService.schedule(new MyRunnable("thread"),10, TimeUnit.SECONDS);
+
+    }
+
+
+    static class MyRunnable implements Runnable {
+        private String name;
+
+        public MyRunnable(String name) {
+            this.name = name;
+        }
+
+
+        @Override
+        public void run() {
+
+            for (int i = 0; i <= 100; i++) {
+
+                System.out.println(Thread.currentThread().getName() + name + ":" + i);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            System.out.println("结束");
+        }
+    }
+
+
+}
