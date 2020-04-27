9193f50586ff0a26956f5042652d8510cf2836d7
haizhilangzi <415242678@qq.com>
2020-04-27 08:38:42 +0800

    init

---
+++ rookie-javase-jvm/src/main/java/com/rookie/bigdata/TMain.java
@@ -0,0 +1,23 @@
+package com.rookie.bigdata;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author rookie
+ * @version 1.0
+ * @date 2020/4/27 8:20
+ */
+public class TMain {
+
+    public static void main(String[] args) {
+
+
+        List<LongMain> longMainList = new ArrayList<>();
+
+        while (true){
+            longMainList.add(new LongMain());
+        }
+
+    }
+}
